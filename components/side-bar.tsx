9a33cfa51140dded783744f300f5fd08c9c11fd1
leruaa <aurelien.catinon@gmail.com>
2023-12-17 18:07:31 +0000

    Handle active links

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -1,23 +1,31 @@
 'use client';
 
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
-import Link from 'next/link'
+import NextLink from 'next/link'
+import { usePathname } from 'next/navigation';
 
 export default () => (
   <NavigationMenu.Root asChild={true}>
     <nav>
       <NavigationMenu.List>
         <NavigationMenu.Item>
-          <NavigationMenu.Link asChild={true}>
-            <Link href="/learn">Overview</Link>
-          </NavigationMenu.Link>
+          <Link href="/learn">Overview</Link>
         </NavigationMenu.Item>
         <NavigationMenu.Item>
-          <NavigationMenu.Link asChild={true}>
-            <Link href="/learn/install">Installation</Link>
-          </NavigationMenu.Link>
+          <Link href="/learn/install">Installation</Link>
         </NavigationMenu.Item>
       </NavigationMenu.List>
     </nav>
   </NavigationMenu.Root>
 )
+
+const Link = ({ href, children }: { href: string, children: React.ReactNode }) => {
+  const pathname = usePathname()
+  const isActive = pathname === href;
+
+  return (
+    <NavigationMenu.Link asChild={true} active={isActive}>
+      <NextLink className="data-[active]:font-semibold" href={href}>{children}</NextLink>
+    </NavigationMenu.Link>
+  );
+};
