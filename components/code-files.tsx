6c06a53ce56ed9cc31a69835dffa5e8ed2e4f153
leruaa <aurelien.catinon@gmail.com>
2023-12-28 21:30:24 +0000

    Display code and result side by side

---
+++ components/code-files.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react';
+
+export default ({ code, files }: { code: ReactElement, files: ReactElement }) => (
+  <div className="flex text-gray-100 bg-gray-700 rounded mb-4">
+    <div className="flex-1">{code}</div>
+    <div className="flex-initial w-0.5 bg-gray-600 my-4"></div>
+    <div className="flex-initial w-96 p-4">{files}</div>
+  </div>
+)
